735fe5367a0146ec5ed8da7aa2ed85199c594568
AndriaGvaramadze <gvaramadzeandria@gmail.com>
2024-08-23 16:10:11 +0400

    commit

---
+++ GOA/Level 16/ClassWork/part2.py
@@ -0,0 +1,10 @@
+for i in range (10 + 1):
+    print (i)
+
+for i in range (0,10+1,2):
+    print(i)
+      
+
+
+for i in range (0,12+1,3):
+    print(i)
---
+++ GOA/Level 16/ClassWork/part3.py
@@ -0,0 +1,11 @@
+#identacia aris for i in range kodis daweris shemdeg dashoreba magalitad tu vwert kods for i in range (5) am kodshi shecdoma is iqneba rom frcxilis daxurvis shemdeg : es simbolo ar
+#  uweria identacia ki is aris rom magalitad igive kods rom davwert ratqmaunda gvchirdeba rom davwerot print magram tu printsa da magla mocemul kods shoris 4 spacet dashoreba ar aris mashin kodi amoagdebs erors magalitad
+
+
+#araswori 
+for i in range (5)
+print(i)
+
+#swori
+for i in range (5):
+    print (i)
