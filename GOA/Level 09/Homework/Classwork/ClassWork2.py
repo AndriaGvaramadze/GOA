e47b779fe659f00052e33b0dba92e8bf6dd0d5a6
AndriaGvaramadze <gvaramadzeandria@gmail.com>
2024-09-13 11:35:25 +0400

    Delete GOA/Level 09/Homework directory

--- GOA/Level 09/Homework/Classwork/ClassWork2.py
+++
@@ -1,3 +0,0 @@
-wins = float(input("enter number of wins: "))
-ties = float(input("enter number of ties: "))
-print("score = " + wins + ties )
--- GOA/Level 09/Homework/Classwork/Classwork.py
+++
@@ -1,2 +0,0 @@
-savings = float (input("enter your savings: "))
-savings_after_1_year = print(savings * 0.05 + savings)
