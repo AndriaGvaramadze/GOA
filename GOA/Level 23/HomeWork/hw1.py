2f7e83e0d18d73c09ce37e0a736984017f93ff3c
AndriaGvaramadze <gvaramadzeandria@gmail.com>
2024-10-26 03:11:03 +0400

    commit

---
+++ GOA/Level 23/HomeWork/hw1.py
@@ -0,0 +1,14 @@
+def check_number():
+ 
+    number = float(input("Sheiyvanet ricxvi: "))
+
+    if number > 0:
+        print("es ricxvi aris dadebiti.")
+    elif number < 0:
+        print("es ricxvi aris uaryofiti.")
+    else:
+        print("es ricxvi aris 0.")
+
+
+check_number()
+    
