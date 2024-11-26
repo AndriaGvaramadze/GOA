6d9c2e8897547d9719b835fa8442a3e9c0a939e9
AndriaGvaramadze <gvaramadzeandria@gmail.com>
2024-11-26 19:45:01 +0400

    Delete GOA/Level 28/HomeWork directory

--- GOA/Level 28/HomeWork/hw2.py
+++
@@ -1,3 +0,0 @@
-def get_grade(s1, s2, s3):
-    # Code here
-    return "F"
--- GOA/Level 28/HomeWork/hw3.py
+++
@@ -1,7 +0,0 @@
-def area_or_perimeter(length, width):
-    if length == width:
-        # If it is a square, return the area
-        return length * width
-    else:
-        # If it is a rectangle, return the perimeter
-        return 2 * (length + width)
